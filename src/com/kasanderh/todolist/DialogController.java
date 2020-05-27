2944426f398d51b0099c40c4ff3ed02a87ef3a75
Kasanderh <Kasanderh@gmail.com>
2020-05-27 12:46:24 +0200

    Initial commit

---
+++ src/com/kasanderh/todolist/DialogController.java
@@ -0,0 +1,33 @@
+package com.kasanderh.todolist;
+
+import com.kasanderh.todolist.datamodel.TodoData;
+import com.kasanderh.todolist.datamodel.TodoItem;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.time.LocalDate;
+
+public class DialogController {
+
+    @FXML
+    private TextField shortDescriptionField;
+
+    @FXML
+    private TextArea detailsArea;
+
+    @FXML
+    private DatePicker deadlinePicker;
+
+    @FXML
+    public TodoItem processResults() {
+        String shortDescription = shortDescriptionField.getText().trim();
+        String details = detailsArea.getText().trim();
+        LocalDate deadlineValue = deadlinePicker.getValue();
+
+        TodoItem newItem = new TodoItem(shortDescription, details,deadlineValue);
+        TodoData.getInstance().addTodoItem(newItem);
+        return newItem;
+    }
+}
