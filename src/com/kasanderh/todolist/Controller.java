88b89b37d77f2243b6bafb06b7ad2063fe644998
Kasanderh <Kasanderh@gmail.com>
2020-05-28 15:50:50 +0200

    Final version.

--- src/com/kasanderh/todolist/Controller.java
+++ src/com/kasanderh/todolist/Controller.java
@@ -212,7 +212,6 @@ public class Controller {
             todoListView.getSelectionModel().select(selectedItem);
         }
     }
-
     @FXML
     public void handleExit() {
         Platform.exit();
